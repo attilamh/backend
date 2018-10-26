5c827598d38ebabadc00803a7e5223dda6eed9a4
Attila Mezei-Horvati <AMezei-Horvati@web.com>
2018-10-26 18:15:56 -0400

    Simplify the email sending to the simple google sendmail. Fix test path.

--- authserver/src/main/java/com/webapp/auth/service/EmailService.java
+++ authserver/src/main/java/com/webapp/auth/service/EmailService.java
@@ -1,50 +1,14 @@
 package com.webapp.auth.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-
-import com.google.api.client.auth.oauth2.Credential;
 
 @Service
 public class EmailService {
 	@Autowired
 	private JavaMailSender emailSender;
-/*
-	 /** Authorizes the installed application to access user's protected data. *
-	 private static Credential authorize() throws Exception {
-	   // load client secrets
-	   GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
-	       new InputStreamReader(EmailService.class.getResourceAsStream("/client_secrets.json")));
-	   // set up authorization code flow
-	   GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-	       httpTransport, JSON_FACTORY, clientSecrets,
-	       Collections.singleton(CalendarScopes.CALENDAR)).setDataStoreFactory(dataStoreFactory)
-	      .build();
-	   // authorize
-	   return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
-	}
-	private static HttpTransport httpTransport;
-	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-	GoogleClientSecrets clientSecrets;
-	GoogleAuthorizationCodeFlow flow;
-	private static com.google.api.services.gmail.Gmail client;
-
-*/	 
-
-	Credential credential;
-
-	@Value("${gmail.client.clientId}")
-	private String clientId;
-
-	@Value("${gmail.client.clientSecret}")
-	private String clientSecret;
-
-	@Value("${gmail.client.redirectUri}")
-	private String redirectUri;	
-	
 	
 	public void sendSimpleMessage(String to, String subject, String text) {
 		SimpleMailMessage message = new SimpleMailMessage(); 
@@ -53,22 +17,5 @@ public class EmailService {
 		message.setText(text);
 		emailSender.send(message);
 	}
-	/*
-	private String authorize() throws Exception {
-		AuthorizationCodeRequestUrl authorizationUrl;
-		if (flow == null) {
-			Details web = new Details();
-			web.setClientId(clientId);
-			web.setClientSecret(clientSecret);
-			clientSecrets = new GoogleClientSecrets().setWeb(web);
-			httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-			flow = new GoogleAuthorizationCodeFlow.Builder(httpTransport, JSON_FACTORY, clientSecrets,
-					Collections.singleton(GmailScopes.GMAIL_READONLY)).build();
-		}
-		authorizationUrl = flow.newAuthorizationUrl().setRedirectUri(redirectUri);
 
-		System.out.println("gamil authorizationUrl ->" + authorizationUrl);
-		return authorizationUrl.build();
-	}
-	*/
 }
--- authserver/src/test/java/com/webapp/auth/AuthApplicationTests.java
+++ authserver/src/test/java/com/webapp/auth/AuthApplicationTests.java
@@ -1,4 +1,4 @@
-package com.web.auth;
+package com.webapp.auth;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
