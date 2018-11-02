9e6be59bccae361da28e91ea226e9f02d8e65737
Attila Mezei-Horvati <AMezei-Horvati@web.com>
2018-11-01 22:07:02 -0400

    Enable logging.

--- authserver/src/main/java/com/webapp/auth/service/EmailService.java
+++ authserver/src/main/java/com/webapp/auth/service/EmailService.java
@@ -1,6 +1,9 @@
 package com.webapp.auth.service;
 
+import java.util.logging.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -9,11 +12,15 @@ import org.springframework.stereotype.Service;
 public class EmailService {
 	@Autowired
 	private JavaMailSender emailSender;
-	
-	public void sendSimpleMessage(String to, String subject, String text) {
-		SimpleMailMessage message = new SimpleMailMessage(); 
-		message.setTo(to); 
-		message.setSubject(subject); 
+
+	private static final Logger logger = Logger.getLogger(EmailService.class.getName());
+
+	public void sendSimpleMessage(String to, String subject, String text) throws MailException {
+		logger.info("Registration message:" + text);
+
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo(to);
+		message.setSubject(subject);
 		message.setText(text);
 		emailSender.send(message);
 	}
